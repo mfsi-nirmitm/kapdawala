cb9046000c6be778b18b292de55bd1c60a0d4884
Nirmit Maheshwari <24971496+mfsi-nirmitm@users.noreply.github.com>
2019-03-05 19:56:28 +0530

    setup project

---
+++ kapdawala/src/main/java/com/kapdashop/kapdawala/KapdawalaApplication.java
@@ -0,0 +1,13 @@
+package com.kapdashop.kapdawala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KapdawalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KapdawalaApplication.class, args);
+	}
+
+}
---
+++ kapdawala/src/test/java/com/kapdashop/kapdawala/KapdawalaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kapdashop.kapdawala;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KapdawalaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
